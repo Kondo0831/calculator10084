4b1bee77e543f35f537934e0a344c097c7c3b6fb
Kondo0831 <ayaka.kondo@pathoslogos.co.jp>
2025-04-10 15:37:11 +0900

    ボタンぼちぼち

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,7 +17,7 @@ describe('AppComponent', () => {
   it(`should have the 'calculator-app' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('calculator-app');
+    expect(app.display).toEqual('calculator-app');
   });
 
   it('should render title', () => {
